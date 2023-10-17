e39508e99f2dde3a592d6e121df1727b6f89c73e
Abglaa <Altymysbaevabdulaziz@gmail.com>
2023-10-18 00:08:38 +0600

    dz3

--- js/lesson.js
+++ js/lesson.js
@@ -16,4 +16,45 @@ phoneButton.addEventListener('click', () => {
     }
 })
 
+// Tab slider
 
+const tabContentBLocks = document.querySelectorAll('.tab_content_block')
+const tabsItems = document.querySelectorAll('.tab_content_item')
+const parentTabs = document.querySelector('.tab_content_items')
+
+
+const hideTabContent = () =>{
+    tabContentBLocks.forEach((tabContentBlock) =>{
+        tabContentBlock.style.display = 'none'
+    })
+    tabsItems.forEach((tabItem)=>{
+        tabItem.classList.remove('tab_content_item_active')
+    })
+}
+const showTabContent = (indexElement = 0) =>{
+    tabContentBLocks[indexElement].style.display = 'block'
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+}
+parentTabs.onclick = (event) =>{
+    if(event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem,tabIndex) =>{
+            if(event.target === tabItem){
+                hideTabContent()
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+const autoTabContentSlide = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabContentBLocks.length - 1) {
+            i = 0
+        }
+        hideTabContent()
+        showTabContent(i)
+    }, 3000)
+}
+autoTabContentSlide()
+hideTabContent()
+showTabContent()
---
+++ js/modal.js
@@ -0,0 +1,45 @@
+//MODAL
+
+const footer = document.querySelector('.footer')
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const closeModalBtn = document.querySelector('.modal_close')
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+
+modalTrigger.onclick = () => openModal()
+closeModalBtn.onclick = () => closeModal()
+modal.onclick = (event) => {
+    if (event.target === modal || event.target === closeModalBtn) closeModal()
+}
+
+let modalOpened = false
+hideModal = () => {
+    openModal()
+    window.removeEventListener("scroll", footerScroll)
+}
+
+footerScroll = () => {
+    if (window.scrollY + window.innerHeight >= footer.offsetTop) {
+        autoModal()
+    }
+}
+
+const autoModal = () => {
+    if (!modalOpened) {
+        modalOpened = true
+        openModal()
+        window.removeEventListener("scroll", footerScroll)
+    }
+}
+
+window.addEventListener("scroll", footerScroll)
+setTimeout(autoModal, 10000)
